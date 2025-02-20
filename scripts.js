e5efe2dc81386d9b9baee38c66364571b17f5f3f
Vitor Santos <vg452004@gmail.com>
2025-02-20 13:17:32 -0300

    ToDo finalizado

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const button = document.querySelector('.btn-task')
+const input = document.querySelector('.input-task')
+const ul = document.querySelector('.list-tasks')
+
+let toDoList = []
+
+function addTask(){
+    if (input.value){
+        toDoList.push({
+            taskName: input.value,
+            completed: false
+        })
+        showTasks()
+    }else{
+        alert('Não é possível salvar uma tarefa vazia!')
+    }
+}
+
+function showTasks(){
+    let newLi = ''
+
+    toDoList.forEach((task, index)=>{
+        newLi += `
+
+        <li class="task ${task.completed?'done':'notdone'}">
+            <img src="img/checked.png" alt="check-na-tarefa" onclick="completeTask(${index})">
+            <p>${task.taskName}</p>
+            <img src="img/trash.png" alt="remover-tarefa" onclick="removeTask(${index})">
+        </li>
+
+        `
+    })
+
+    ul.innerHTML = newLi
+
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+}
+
+function removeTask(index){
+    toDoList.splice(index, 1)
+    showTasks()
+}
+
+function completeTask(index){
+    toDoList[index].completed = !toDoList[index].completed
+    showTasks()
+}
+
+function reloadTasks(){
+    const localStorageTasks = localStorage.getItem('toDoList')
+    if (localStorageTasks){
+        toDoList = JSON.parse(localStorageTasks)
+    }
+    showTasks()
+}
+
+reloadTasks()
+button.addEventListener('click', addTask)
